d24cec52e03dd4cd1d7bd28c8a230d3845e570d7
pacosw1 <pacoswilliams@gmail.com>
2020-09-28 14:38:59 -0500

    Updates

---
+++ precache-manifest.45be818c224b7d3f376e439c6add60a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16518db562c9e32b77661ea80615ef36",
+    "url": "/rick-morty-generator/index.html"
+  },
+  {
+    "revision": "bce2af92ffc8606578fa",
+    "url": "/rick-morty-generator/static/css/main.71ded0da.chunk.css"
+  },
+  {
+    "revision": "4d344caba5dfa15aa1d5",
+    "url": "/rick-morty-generator/static/js/2.a5c42eab.chunk.js"
+  },
+  {
+    "revision": "830d9a3a4453202f63a1e0cb3a98506f",
+    "url": "/rick-morty-generator/static/js/2.a5c42eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce2af92ffc8606578fa",
+    "url": "/rick-morty-generator/static/js/main.5f2af41e.chunk.js"
+  },
+  {
+    "revision": "8b00cb0431145ed4ce4a",
+    "url": "/rick-morty-generator/static/js/runtime-main.b964516e.js"
+  }
+]);
